15ce11f2b4913276a7fdd16ca49e5ae56d7e2bf8
Salamon Csongor <csongorawot1@gmail.com>
2025-05-24 17:28:49 +0300

    #7 - created user view and edit pages

--- app/Domain/User/Filament/Admin/Resources/UserResource/Pages/ViewUser.php
+++ app/Domain/User/Filament/Admin/Resources/UserResource/Pages/ViewUser.php
@@ -3,7 +3,11 @@
 namespace App\Domain\User\Filament\Admin\Resources\UserResource\Pages;
 
 use App\Domain\User\Filament\Admin\Resources\UserResource;
+use App\Domain\User\Models\User;
 use Filament\Actions;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewUser extends ViewRecord
@@ -12,6 +16,40 @@ class ViewUser extends ViewRecord
      * @var string $resource The resource that this page is associated with.
      */
     protected static string $resource = UserResource::class;
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make(__('User Information'))
+                    ->schema([
+                        TextEntry::make('name')
+                            ->label(__('Name')),
+                        TextEntry::make('email')
+                            ->label(__('Email')),
+                        TextEntry::make('email_verified_at')
+                            ->label(__('Email Verified At'))
+                            ->placeholder(__('Not Verified'))
+                            ->dateTime(),
+                    ])
+                    ->columns(['md' => 2])
+                    ->columnSpan(['md' => 2]),
+                Section::make()
+                    ->schema([
+                        TextEntry::make('created_at')
+                            ->label(__('Created At'))
+                            ->dateTime(),
+                        TextEntry::make('updated_at')
+                            ->label(__('Updated At'))
+                            ->dateTime(),
+                        TextEntry::make('deleted_at')
+                            ->label(__('Deleted At'))
+                            ->dateTime()
+                            ->color('danger')
+                            ->visible(fn (User $record) => $record->trashed()),
+                    ])->columnSpan(['lg' => 1]),
+            ])->columns(3);
+    }
 
     /**
      * Get the header actions for the view user page.
