efe6d47f85d7a2d5b03eb31230308b32bbeb0c9d
Salamon Csongor <csongorawot1@gmail.com>
2025-05-24 12:03:18 +0300

    #7 - created admin user

---
+++ app/Domain/Admin/Models/Admin.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Domain\Admin\Models;
+
+use Database\Factories\AdminFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends User implements MustVerifyEmail
+{
+    /** @use HasFactory<\Database\Factories\AdminFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return \Database\Factories\AdminFactory
+     */
+    protected static function newFactory(): AdminFactory
+    {
+        return AdminFactory::new();
+    }
+}
