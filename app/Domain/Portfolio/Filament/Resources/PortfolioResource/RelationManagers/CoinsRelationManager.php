cdb3a7df821993ab863e2309022a2edf9e40f38b
Salamon Csongor <csongorawot1@gmail.com>
2025-05-27 18:13:52 +0300

    #15 - added amount editing option to uer

--- app/Domain/Portfolio/Filament/Resources/PortfolioResource/RelationManagers/CoinsRelationManager.php
+++ app/Domain/Portfolio/Filament/Resources/PortfolioResource/RelationManagers/CoinsRelationManager.php
@@ -2,9 +2,14 @@
 
 namespace App\Domain\Portfolio\Filament\Resources\PortfolioResource\RelationManagers;
 
+use App\Domain\Coin\Models\Coin;
+use Auth;
 use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\ActionGroup;
 use Filament\Tables\Actions\AttachAction;
 use Filament\Tables\Actions\DetachAction;
 use Filament\Tables\Table;
@@ -57,10 +62,35 @@ class CoinsRelationManager extends RelationManager
                     ])
             ])
             ->actions([
-                DetachAction::make()
-                    ->label('Remove Coin')
-                    ->color('danger')
-                    ->requiresConfirmation(),
+                ActionGroup::make([
+                    Action::make('edit_amount')
+                        ->label('Edit Amount')
+                        ->icon('heroicon-o-pencil')
+                        ->color('primary')
+                        ->requiresConfirmation()
+                        ->authorize(fn (Coin $record) => Auth::user()->can('changeAmount', $record))
+                        ->form([
+                            TextInput::make('pivot.amount')
+                                ->label('Amount Held')
+                                ->numeric()
+                                ->required()
+                                ->default(fn (Coin $record) => $record->pivot->amount)
+                                ->minValue(0),
+                        ])
+                        ->action(function (Coin $record, array $data) {
+                            $record->pivot->update(['amount' => $data['pivot']['amount']]);
+
+                            Notification::make()
+                                ->title('Amount Updated')
+                                ->body("The amount for {$record->name} has been updated to {$data['pivot']['amount']}.")
+                                ->success()
+                                ->send();
+                        }),
+                    DetachAction::make()
+                        ->label('Remove Coin')
+                        ->color('danger')
+                        ->requiresConfirmation(),
+                ])
             ]);
     }
 
